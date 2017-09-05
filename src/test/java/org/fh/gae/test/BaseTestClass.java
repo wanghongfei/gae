48c5364c78f35f50242686e25c7e42686e7580aa
wanghongfei <brucewhf@gmail.com>
2017-09-05 23:00:56 +0800

    spring boot单元测试环境

---
+++ src/test/java/org/fh/gae/test/BaseTestClass.java
@@ -0,0 +1,11 @@
+package org.fh.gae.test;
+
+import org.fh.gae.GaeApp;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = GaeApp.class)
+public class BaseTestClass {
+}
---
+++ src/test/java/org/fh/gae/test/index/DataTableTest.java
@@ -0,0 +1,15 @@
+package org.fh.gae.test.index;
+
+import org.fh.gae.query.index.DataTable;
+import org.fh.gae.query.index.unit.AdUnitIndex;
+import org.fh.gae.test.BaseTestClass;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DataTableTest extends BaseTestClass {
+    @Test
+    public void testOf() {
+        int len = DataTable.of(AdUnitIndex.class).getLevel();
+        Assert.assertEquals(len, AdUnitIndex.LEVEL);
+    }
+}
